c5a2c67dec24115680d9265ae41bc53984a47aae
danielrpg <daniel.fernandezgp@gmail.com>
2019-05-23 22:58:13 -0400

    Add bootstrap and add list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  Component{
+
+  constructor() {
+    
+    super();
+    this.state = {
+      
+      todos: [{
+        id: 1,
+        name: 'Play golf'
+      },{
+        id: 2,
+        name: 'Buy some clothes'
+      },{
+        id:3,
+        name: 'Write some code'
+      }]
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            CRUD
+          </p>
+        </header>
+        <div className="container">
+          <h2 className="text-center p-4">Todos App</h2>
+          <ul className="list-group">
+            {this.state.todos.map((item) =>{
+              return <li className="list-group-item">{item.name}</li>
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
