68b6351ac88348c7f4876dc85d31a0764f6d3cf3
danielrpg <daniel.fernandezgp@gmail.com>
2019-05-30 23:37:45 -0400

    Changes on addTodo Method

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends  Component{
     
     super();
     this.state = {
+      newTodo:'',
       
       todos: [{
         id: 1,
@@ -21,9 +22,38 @@ class App extends  Component{
       }]
     }
 
+    //this.addTodo = this.addTodo.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      
+      newTodo: event.target.value
+
+    });
+  }
+
+  addTodo = () => {
+    const newTodo = {
+      name: this.state.newTodo,
+      id: this.state.todos[this.state.todos.length - 1].id + 1 
+    };
+
+    const oldTodos = this.state.todos;
+    oldTodos.push(newTodo);
+
+    this.setState ({
+        todos: oldTodos,
+        newTodo: ''
+    });
+    // the state is INMUTABLE
   }
 
   render() {
+
+    console.log(this.state.newTodo);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -33,10 +63,26 @@ class App extends  Component{
           </p>
         </header>
         <div className="container">
+          
           <h2 className="text-center p-4">Todos App</h2>
+
+          <input
+            type="text"
+            className="my-4 form-control"
+            placeholder="Add new todo"
+            onChange={this.handleChange}
+            value={this.state.newTodo}/>
+
+          <button
+            onClick={this.addTodo} 
+            className="btn-info mb-3 form-control">
+            Add Todo
+          </button>
+
+
           <ul className="list-group">
-            {this.state.todos.map((item) =>{
-              return <li className="list-group-item">{item.name}</li>
+            {this.state.todos.map((item, index) =>{
+              return <li key={item.id} className="list-group-item">{item.name}</li>
             })}
           </ul>
         </div>
